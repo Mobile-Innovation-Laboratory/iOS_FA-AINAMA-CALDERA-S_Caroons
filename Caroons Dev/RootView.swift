8d98daae203ce805549205ae3db12e525c3507a3
Facalder <faainamacaldera16@gmail.com>
2025-03-04 09:11:36 +0700

    Feat(Scaffholding): Scaffholding project and project structuring

---
+++ Caroons Dev/RootView.swift
@@ -0,0 +1,14 @@
+//
+//  RootView.swift
+//  Caroons Dev
+//
+//  Created by Fa Ainama Caldera S  on 04/03/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
